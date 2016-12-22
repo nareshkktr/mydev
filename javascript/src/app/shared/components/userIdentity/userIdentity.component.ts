c30330a72b261c747cc692abcfeb288fbc92ba18
Sripriya Venkatesh Prasad <Sripriya@Sripriyas-MacBook-Pro.local>
2016-12-23 05:01:48 +0530

    Pushing service INT for user identity

--- javascript/src/app/shared/components/userIdentity/userIdentity.component.ts
+++ javascript/src/app/shared/components/userIdentity/userIdentity.component.ts
@@ -12,49 +12,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class UserIdentityComponent{
 
-  @Input() voterId: any;
-  @Input() name: any;
-  @Output() userIdentityStatus = new EventEmitter<any>();
-
-  private userData;
-
-  private eventResponse;
-
   private userIdentityForm: any;
 
+  @Input() electorID :string;
+  @Input() electorName: string;
+
   constructor(private userIdentityService:UserIdentityService,private router: Router,fb: FormBuilder) {
-  	this.userData = {};
     this.userIdentityForm = fb.group({
-      loginUserName: [null,Validators.required],
-      loginUserPassword: [null,Validators.required]
+      electorID: [null,Validators.required],
+      electorName: [null,Validators.required]
     });
   }
 
-validateElectorInfo(){
-	this.router.navigate(['../signUp/userValidation']);
-}
+  validateElectorInfo(){
+  	this.router.navigate(['../signUp/userValidation']);
+  }
 
   retreiveUser(){
-  	this.userIdentityService.retreiveUser(this.voterId,this.name)
-                           .subscribe(
-                               userData => {
-                               	this.userData = userData;
-                               	this.eventResponse = {
-                               		status: true,
-                               		userData: this.userData
-                               	}
-                               	this.userIdentityStatus.emit(this.eventResponse);
-                               }, //Bind to view
- 							
-                                err => {
-                                    // Log errors if any
-                                    console.log(err);
-                                    this.eventResponse = {
-                               			status: false,
-                               			errorMsg: err
-                               		}
-                                    this.userIdentityStatus.emit(this.eventResponse);
-                                });
+
+    this.userIdentityService.retreiveUser(this.electorID,this.electorName).subscribe(result =>{
+              console.log(JSON.parse(result));
+              this.router.navigate(['../signUp/userValidation']);
+              alert("Login Success");
+        },
+        error => {
+              alert(error.statusText);
+        });
   }
 
 }
