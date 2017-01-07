9c780bb5b0179fd9848cce2007351970b5e77b45
unknown <VENKASB@WKC001335718.ap.cat.com>
2017-01-07 17:25:35 +0530

    Pushing model file

---
+++ src/main/java/com/kasisripriyawebapps/myindia/requestresponsemodel/PoliticianExportModel.java
@@ -0,0 +1,42 @@
+package com.kasisripriyawebapps.myindia.requestresponsemodel;
+
+public class PoliticianExportModel {
+	
+	private String politicianName;
+	private String partyName;
+	private String locationName;
+	private String designation;
+	private String politicianType;
+	
+	public String getPoliticianName() {
+		return politicianName;
+	}
+	public void setPoliticianName(String politicianName) {
+		this.politicianName = politicianName;
+	}
+	public String getPartyName() {
+		return partyName;
+	}
+	public void setPartyName(String partyName) {
+		this.partyName = partyName;
+	}
+	public String getLocationName() {
+		return locationName;
+	}
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getPoliticianType() {
+		return politicianType;
+	}
+	public void setPoliticianType(String politicianType) {
+		this.politicianType = politicianType;
+	}
+
+}
