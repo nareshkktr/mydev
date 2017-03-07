ab28a534ff4fd2ab87a8d649202b97fa336863db
unknown <venkasb@WKC001335718.ap.cat.com>
2017-03-07 15:12:21 +0530

    Pushing file upload code

--- javascript/a1/app/views/problem/createProblemController.js
+++ javascript/a1/app/views/problem/createProblemController.js
@@ -3,9 +3,9 @@
 
 	angular.module('myindia-app').controller("createProblemController",
 			createProblemController);
-	createProblemController.$inject = [ 'createProblemService' ];
+	createProblemController.$inject = [ 'createProblemService','fileUploadService' ];
 	
-	function createProblemController(createProblemService) {
+	function createProblemController(createProblemService,fileUploadService) {
 		
 		var createProblem=this;
 		createProblem.problemTypesResults = [];
@@ -28,8 +28,19 @@
 			}
 		}
 
-		function uploadCover(file){
-			createProblem.coverPhotoFile = file[0];
+		function uploadCover(files){
+			createProblem.coverPhotoFile = files[0];
+			
+			// Move it to  a place where u are going to create problem.
+			fileUploadService.uploadFile(files,12,"Problem").then(uploadSuccess).catch(uploadFailure);
+
+			function uploadSuccess(data){
+				console.log(data);
+			}
+
+			function uploadFailure(error){
+				alert(error);
+			}
 		}
 	}
 
