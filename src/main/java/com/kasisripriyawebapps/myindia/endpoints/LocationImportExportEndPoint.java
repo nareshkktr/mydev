d3d463f21e569e66c8d25dd8e21d47eb82ebd25a
AP\kanduk <kandula_kasireddy@cat.com>
2017-04-13 15:27:29 +0530

    Locations Import and Export Storing Destination Changed From  Local To Amazon S3

--- src/main/java/com/kasisripriyawebapps/myindia/endpoints/LocationImportExportEndPoint.java
+++ src/main/java/com/kasisripriyawebapps/myindia/endpoints/LocationImportExportEndPoint.java
@@ -1,16 +1,10 @@
 package com.kasisripriyawebapps.myindia.endpoints;
 
-import java.io.InputStream;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.kasisripriyawebapps.myindia.constants.ApplicationConstants;
