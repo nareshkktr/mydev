188b454c3588f31c7661ffb0813739755a6fc267
unknown <patchn@WKC001705210.ap.cat.com>
2017-04-15 10:14:01 +0530

    ThumbsUpDown service added

--- src/main/java/com/kasisripriyawebapps/myindia/dao/ThumbsUpDownDao.java
+++ src/main/java/com/kasisripriyawebapps/myindia/dao/ThumbsUpDownDao.java
@@ -3,9 +3,13 @@
  */
 package com.kasisripriyawebapps.myindia.dao;
 
+import com.kasisripriyawebapps.myindia.entity.ThumbsUpDown;
+import com.kasisripriyawebapps.myindia.exception.InternalServerException;
+
 /**
  * The Interface ThumbsUpDownDao.
  */
 public interface ThumbsUpDownDao {
+	Long createThumbsUpDown(ThumbsUpDown thumbsUpDown) throws InternalServerException;
 
 }
--- src/main/java/com/kasisripriyawebapps/myindia/daoimpl/ThumbsUpDownDaoImpl.java
+++ src/main/java/com/kasisripriyawebapps/myindia/daoimpl/ThumbsUpDownDaoImpl.java
@@ -7,11 +7,17 @@ import org.springframework.stereotype.Repository;
 
 import com.kasisripriyawebapps.myindia.dao.ThumbsUpDownDao;
 import com.kasisripriyawebapps.myindia.entity.ThumbsUpDown;
+import com.kasisripriyawebapps.myindia.exception.InternalServerException;
 
 /**
  * The Class ThumbsUpDownDaoImpl.
  */
 @Repository
 public class ThumbsUpDownDaoImpl extends BaseDaoImpl<Long, ThumbsUpDown> implements ThumbsUpDownDao {
+
+	@Override
+	public Long createThumbsUpDown(ThumbsUpDown thumbsUpDown) throws InternalServerException {
+		return save(thumbsUpDown);
+	}
 
 }
